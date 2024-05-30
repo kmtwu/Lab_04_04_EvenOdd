358cb425919a362bbe9e79f14a90a328393c0c08
kmtwu <mr.kwu13579@gmail.com>
2024-05-30 18:09:36 -0400

    done

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 6;
+        int moduloOfTwo;
+        moduloOfTwo = numToExamine % 2;
+        System.out.println("Since the mod 2 of " + numToExamine + " is " + moduloOfTwo + ", " + numToExamine + " is even.");
+    }
+}
